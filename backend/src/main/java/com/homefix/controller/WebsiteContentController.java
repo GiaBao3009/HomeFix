11637f0711391240fb62984ec2c142f024b6aaeb
GiaBao3009 <baoldz3009@gmail.com>
2026-03-31 04:57:22 +0700

    Fix: production deployment: CORS, healthcheck, network

--- backend/src/main/java/com/homefix/controller/WebsiteContentController.java
+++ backend/src/main/java/com/homefix/controller/WebsiteContentController.java
@@ -4,14 +4,17 @@ import com.homefix.entity.WebsiteContent;
 import com.homefix.service.WebsiteContentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/content")
-@CrossOrigin(origins = "http://localhost:5173")
 public class WebsiteContentController {
 
     @Autowired
